3a0495de48402034736433a1191d164b81c37259
ghofarassidik@gmail.com <ghofarassidik@gmail.com>
2024-05-22 15:20:06 +0700

    feat: home page basic

---
+++ apps/web/src/components/module/Navbar.jsx
@@ -0,0 +1,42 @@
+import Logo from "../images/logo/blanja.png";
+import Search from "../images/logo/search.png";
+import Sort from "../images/logo/filter.png";
+import Cart from "../images/logo/cart.png";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full h-[100px] mx-auto flex items-center bg-white shadow-md">
+      <img className="ml-[50px] container:ml-[150px]" src={Logo} alt="" />
+
+      <div className="flex items-center flex-grow mx-5">
+        <div className="flex-grow flex rounded-full bg-white border border-abu overflow-hidden">
+          <input
+            className="flex-grow h-[40px] text-base focus:outline-none focus:ring-0 placeholder:text-abu px-4"
+            type="text"
+            placeholder="Search"
+          />
+          <button className="p-2">
+            <img src={Search} alt="Search" />
+          </button>
+        </div>
+        <div className="bg-white h-[40px] w-[40px] border border-abu flex items-center rounded-lg ml-2">
+          <img src={Sort} alt="Sort" className="mx-auto" />
+        </div>
+      </div>
+
+      <div className="flex h-[40px] items-center space-x-[20px] mr-[50px] container:mr-[150px]">
+        <div className="flex items-center h-9 w-[44px]">
+          <img className="mx-auto" src={Cart} alt="Cart" />
+        </div>
+        <button className="h-9 w-[100px] border border-abu text-sm text-abu rounded-full hover:bg-red-700 hover:text-white hover:border-0">
+          Masuk
+        </button>
+        <button className="h-9 w-[100px] border border-abu text-sm text-abu rounded-full hover:bg-red-700 hover:text-white hover:border-0">
+          Daftar
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,9 +1,75 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+import withMT from '@material-tailwind/react/utils/withMT';
+
+export default withMT({
+  content: [
+    './index.html',
+    './src/**/*.{jsx, tsx, js, ts}',
+    '../../node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}',
+    '../../node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+      blanja_roboto: ['Roboto', 'sans-serif'],
+      blanja_metropolis: ['Metropolis', 'sans-serif'],
+    },
+    colors: {
+      main: {
+        red: '#C70039',
+        blue: '#141E46',
+        pink: '#FF6969',
+        light: '#FFF5E0',
+        abu: '#8E8E93',
+      },
+    },
     extend: {},
+    screens: {
+              'xs': '480px',
+              'sm': '640px',
+              'md': '768px',
+              'lg': '1024px',
+              'xl': '1280px',
+              '2xl': '1536px',
+              '3xl': '1920px',
+              'dekstop': '1440px',
+            },
   },
   plugins: [],
-}
+});
+
+
+
+
+
+
+
+
+
+
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       screens: {
+//         'xs': '480px',
+//         'sm': '640px',
+//         'md': '768px',
+//         'lg': '1024px',
+//         'xl': '1280px',
+//         '2xl': '1536px',
+//         '3xl': '1920px',
+//         'dekstop': '1440px',
+//       },
+//       colors:{
+//         'abu': '#8E8E93',
+//       }
+//     },
+//   },
+//   plugins: [],
+// }
 
