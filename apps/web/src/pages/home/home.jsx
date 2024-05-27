058af4e7180314e8a33af308115223119ed36567
ghofarassidik@gmail.com <ghofarassidik@gmail.com>
2024-05-27 12:26:20 +0700

    fix product.ID di home

--- apps/web/src/pages/home/home.jsx
+++ apps/web/src/pages/home/home.jsx
@@ -65,7 +65,7 @@ const Home = () => {
                 product_name={product?.name || ""}
                 price={`$${product?.price}` || ""}
                 store={product?.Store?.name || ""}
-                onClick={() => handleProductDetail(product.id)}
+                onClick={() => handleProductDetail(product.ID)}
               />
             ))
           ) : (
@@ -84,7 +84,7 @@ const Home = () => {
                 product_name={product?.name || ""}
                 price={`$${product?.price}` || ""}
                 store={product?.Store?.name || ""}
-                onClick={() => handleProductDetail(product.id)}
+                onClick={() => handleProductDetail(product.ID)}
               />
             ))}
           </Recommendation>
@@ -100,7 +100,7 @@ const Home = () => {
                 product_name={product?.name || ""}
                 price={`$${product?.price}` || ""}
                 store={product?.Store?.name || ""}
-                onClick={() => handleProductDetail(product.id)}
+                onClick={() => handleProductDetail(product.ID)}
               />
             ))}
           </Recommendation>
