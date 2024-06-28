f329a9ccbd671076e9e55d89593842b293ba17a5
dimassagngsptr <dimasageng58@gmail.com>
2024-06-28 15:59:08 +0700

    fix:cors origin

--- apps/server/main.go
+++ apps/server/main.go
@@ -10,7 +10,7 @@ import (
 	"server/src/routes"
 
 	"github.com/gofiber/fiber/v2"
-	// "github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -20,29 +20,33 @@ func getPort() string {
 		port = "3000"
 	}
 	return "0.0.0.0:" + port
-
 }
+
 func main() {
 	if _, err := os.Stat(".env"); err == nil {
 		if err := godotenv.Load(); err != nil {
 			log.Fatal("Error loading .env file")
 		}
 	}
-	// PORT := 3000
+
 	app := fiber.New()
 
-	// app.Use(cors.New(cors.Config{
-	// 	AllowOrigins:     "https://blanja-kelompok-1.vercel.app",
-	// 	AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
-	// 	AllowHeaders:     "Content-Type, Authorization",
-	// 	ExposeHeaders:    "Content-Length",
-	// 	AllowCredentials: true,
-	// }))
+	// CORS middleware configuration
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "https://blanja-kelompok-1.vercel.app",
+		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders:     "Content-Type, Authorization",
+		ExposeHeaders:    "Content-Length",
+		AllowCredentials: true,
+	}))
 
 	configs.InitDB()
 	services.InitMidtrans()
 	helpers.Migration()
 	routes.Router(app)
-	app.Listen(getPort())
 
+	// Log error if app fails to listen
+	if err := app.Listen(getPort()); err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
 }
